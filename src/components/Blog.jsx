5409e58bbf2402b21184d2a486e53109f5a0d615
Dinesh Rout <rddinesh779@gmail.com>
2022-08-01 22:56:43 +0530

    hogeya

--- src/components/Blog.jsx
+++
@@ -1,9 +0,0 @@
-const Blog = ({ blog }) => {
-  return (
-    <div className='my-4 shadow-md p-4 rounded-md'>
-      <h1 className='text-4xl font-bold'>{blog.title}</h1>
-      <p>{blog.description}</p>
-    </div>
-  );
-};
-export default Blog;
